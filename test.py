00bf805e8a8bfddeb74c9b3565af8e71f3734b19
Tobias Hannaske <tobias.hannaske@de-cix.net>
2016-09-29 08:13:06 +0200

    Remove unneeded test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-#
-# This file is part of PCAP BGP Parser (pbgpp)
-#
-# Copyright (c) 2016 DE-CIX Management GmbH <https://www.de-cix.net> - All rights reserved.
-# Author: Tobias Hannaske <tobias.hannaske@de-cix.net>
-#
-# <LICENSE>
-#
