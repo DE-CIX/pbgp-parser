767afc442a846db8f6fbbcc66cd5e17edda08912
iLLogical2007 <tobias@hannaske.eu>
2016-11-24 10:44:10 +0100

    Fixing version bug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ packages.append('pbgpp')
 
 setup(
     name='pbgpp',
-    version='0.2.4',
+    version='0.2.5',
     description='PCAP BGP Parser',
     author='DE-CIX Management GmbH',
     author_email='rnd@de-cix.net',
