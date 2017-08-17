a3d2ae64a02f36b18086fc96247e483c6dff747d
iLLogical2007 <tobias@hannaske.eu>
2017-08-17 09:57:03 +0200

    Raising version number due to fix of issue #29

--- pbgpp/Application/Handler.py
+++ pbgpp/Application/Handler.py
@@ -81,7 +81,7 @@ class PBGPPHandler:
         logger = logging.getLogger('pbgpp.PBGPPHandler.handle')
 
         if self.args.version:
-            print("pbgpp PCAP BGP Parser v0.2.12")
+            print("pbgpp PCAP BGP Parser v0.2.13")
             print("Copyright 2016-2017, DE-CIX Management GmbH")
             sys.exit(0)
 
